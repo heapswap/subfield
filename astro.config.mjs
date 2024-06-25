909efc0436e67ad7290dc8c7135aa1c5923671e4
kahnpoint <96557890+kahnpoint@users.noreply.github.com>
2024-06-24 20:30:41 -0700

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), mdx(), tailwind()]
+});
